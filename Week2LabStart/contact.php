cc40fee7641bb8d7881c547fad2aeb8645d59f36
coftbred <80310115+coftbred@users.noreply.github.com>
2021-04-23 08:51:34 +0700

    Add files via upload

---
+++ Week2LabStart/contact.php
@@ -0,0 +1,24 @@
+<?php
+  include 'includes/header.php'
+ ?>
+ <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15678.822607365857!2d106.68809554999999!3d10.757153399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1c06f4e1dd%3A0x43900f1d4539a3d!2sVietnam%20National%20University%20Ho%20Chi%20Minh%20City%20-%20University%20of%20Science!5e0!3m2!1sen!2s!4v1619142162152!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+<div class="container">
+  <h2>Contact Us</h2>
+  <hr>
+  <div class="row">
+    <div class="col-md-6">
+      <img src="https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/c1/ae/3d/c1ae3d6d-8629-acd0-6ec6-8fd4e4a1b9b8/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png" alt="">
+    </div>
+    <div class="col-md-6">
+      <h2>Contact form</h2>
+      <form action="index.php" class="" method="POST">
+        <button type="submit" class="btn btn-primary">Sign in</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php
+include 'includes/footer.php'
+ ?>
